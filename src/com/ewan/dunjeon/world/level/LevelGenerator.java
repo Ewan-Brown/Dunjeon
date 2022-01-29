932563d09864786efd17d6609a017877a82ed677
Ewan-Brown <ewanbrown2000@gmail.com>
2022-01-29 14:57:28 -0500

    Fixed mismatch of array indices and x/y coords, all coords now in [y][x]

--- src/com/ewan/dunjeon/world/level/LevelGenerator.java
+++ src/com/ewan/dunjeon/world/level/LevelGenerator.java
@@ -17,9 +17,9 @@ public class LevelGenerator {
     public static Level createLevel(int[][] map){
         BasicCell[][] cells = new BasicCell[map.length][map[0].length];
         Level lev = new Level();
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                int val = map[j][i];
+        for (int y = 0; y < map.length; y++) {
+            for (int x = 0; x < map[0].length; x++) {
+                int val = map[y][x];
                 boolean filled = false;
                 Color c;
                 Furniture f = null;
@@ -33,6 +33,7 @@ public class LevelGenerator {
                         c = Color.LIGHT_GRAY;
                         break;
                     case OPEN:
+                    case HALL:
                         c = Color.GRAY;
                         filled = false;
                         break;
@@ -41,18 +42,14 @@ public class LevelGenerator {
                         f = new Door(false);
                         filled = false;
                         break;
-                    case HALL:
-                        c = Color.GRAY;
-                        filled = false;
-                        break;
                     default:
                         c = Color.RED;
                 }
-                BasicCell cell = new BasicCell(j, i, lev, filled, c);
+                BasicCell cell = new BasicCell(x, y, lev, filled, c);
                 if(f != null) {
                     cell.setFurniture(f);
                 }
-                cells[j][i] = cell;
+                cells[y][x] = cell;
 
             }
         }
