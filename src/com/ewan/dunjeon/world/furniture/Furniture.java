9f3e0c4c896badc03d880e104901cf0ff2809a3e
Ewan Brown <ewanbrown2000@gmail.com>
2022-10-01 13:02:56 -0400

    Removed furniture offset... unnecessary for now

--- src/com/ewan/dunjeon/world/furniture/Furniture.java
+++ src/com/ewan/dunjeon/world/furniture/Furniture.java
@@ -19,12 +19,9 @@ public abstract class Furniture implements Updateable {
     public boolean isBlocking(){
         return false;
     }
-
     public float getSize(){return 0.8f;}
-    public float getOffsetX(){return 0;}
-    public float getOffsetY(){return 0;}
-    public float getCenterX(){return containingCell.getX() + 0.5f + getOffsetX();}
-    public float getCenterY(){return containingCell.getY() + 0.5f + getOffsetY();}
+    public float getCenterX(){return containingCell.getX() + 0.5f;}
+    public float getCenterY(){return containingCell.getY() + 0.5f;}
 
     @Override
     public void update() {
