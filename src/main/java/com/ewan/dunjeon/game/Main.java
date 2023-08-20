d52b531d2600e116494b2e9d8538995954839676
Ewan-Brown <ewanbrown2000@gmail.com>
2023-08-20 09:10:31 -0400

    Updated main to include utilities

--- src/main/java/com/ewan/dunjeon/game/Main.java
+++ src/main/java/com/ewan/dunjeon/game/Main.java
@@ -11,6 +11,9 @@ import com.ewan.dunjeon.world.Dunjeon;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.dyn4j.geometry.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 
@@ -19,17 +22,27 @@ public class Main {
     public static final Random rand = new Random();
     static final long UPDATE_DELAY = 16;
     private static KeyBank keyBank = new KeyBank();
+    private static final int entityCount = 0;
 
     public static void main(String[] args) {
-
 
         System.out.println("Begin");
         generateWorld();
 
-
         new Thread(() -> {
             while (true) {
-                updateCurrentWorld(100);
+                updateCurrentWorld(10);
+                if(all_durations.size() == 1000){
+
+                    try {
+                        PrintWriter outFile = new PrintWriter(new FileWriter("data_" + entityCount+"entities" + ".csv"));
+                        PrintWriter finalOutFile = outFile;
+                        all_durations.forEach(i-> finalOutFile.print(i + ", "));
+                        outFile.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         }).start();
 
@@ -51,6 +64,7 @@ public class Main {
     }
 
     private static List<Long> durations = new ArrayList<>();
+    private static List<Long> all_durations = new ArrayList<>();
 
     private static void updateCurrentWorld(int dataSamples){
         Dunjeon w = Dunjeon.getInstance();
@@ -69,6 +83,7 @@ public class Main {
                 System.out.println("Mean duration : " + stats.getMean());
                 System.out.println("Standard Deviation : " + stats.getStandardDeviation());
                 System.out.println("Skewness : " + stats.getSkewness());
+                all_durations.addAll(durations);
                 durations.clear();
             }
         } catch (InterruptedException e) {
@@ -76,11 +91,24 @@ public class Main {
         }
     }
 
+    private static long DESIRED_FRAMETIME_NS = 16000000;
+
     private static void updateCurrentWorld(){
         Dunjeon w = Dunjeon.getInstance();
         try {
-            Thread.sleep(UPDATE_DELAY);
+            Thread.sleep(DESIRED_FRAMETIME_NS/1000000L);
+//            long l0 = System.nanoTime();
             w.update(1.0D);
+//            long l1 = System.nanoTime();
+//            long diff = l1-l0;
+//            if(diff > DESIRED_FRAMETIME_NS){
+//                System.out.println("=".repeat(20));
+//                System.out.println(diff);
+//                System.out.println(DESIRED_FRAMETIME_NS);
+//                System.err.printf("Game frame took %.2f%% too long!\n", (diff - DESIRED_FRAMETIME_NS)/ DESIRED_FRAMETIME_NS *100D);
+//            }else{
+//
+//            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -123,12 +151,16 @@ public class Main {
 
         startFloor.addCreatureController(new TestSubjectPlayerController(testSubject, keyBank));
 
-//        TestSubject npcTestSubject = new TestSubject("NPC");
-//        npcTestSubject.addFixture(new Rectangle(0.5,0.5));
-//        npcTestSubject.setMass(new Mass(new Vector2(),1,1));
-//        startFloor.addEntityRandomLoc(npcTestSubject);
-//
-//        startFloor.addCreatureController(new TestSubjectAIController(npcTestSubject));
+        for (int i = 0; i < entityCount; i++) {
+            TestSubject npcTestSubject = new TestSubject("NPC");
+            npcTestSubject.addFixture(new Rectangle(0.5,0.5));
+            npcTestSubject.setMass(new Mass(new Vector2(),1,1));
+            startFloor.addEntityRandomLoc(npcTestSubject);
+
+            startFloor.addCreatureController(new TestSubjectAIController(npcTestSubject));
+        }
+
+
 
     }
 }
