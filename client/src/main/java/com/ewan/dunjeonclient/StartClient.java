214565144b4687731ea4404bad236d22655a3409
Ewan-Brown <ewanbrown2000@gmail.com>
2023-11-05 23:58:57 -0500

    launchable jar now gets created correctly
    needs to be run by shell script that provides correct JVM args

--- client/src/main/java/com/ewan/dunjeonclient/StartClient.java
+++ client/src/main/java/com/ewan/dunjeonclient/StartClient.java
@@ -3,9 +3,12 @@ package com.ewan.dunjeonclient;
 import com.esotericsoftware.kryo.kryo5.Kryo;
 import com.esotericsoftware.kryo.kryo5.minlog.Log;
 
+import java.util.Arrays;
+
 public class StartClient
 {
     public static void main(String[] args) {
+        System.out.println("Arrays.toString(args) = " + Arrays.toString(args));
         ClientChannelHandler clientChannelHandler = new ClientChannelHandler();
         new Thread(() -> new GameClient(clientChannelHandler, args[0])).start();
         new Thread(() -> {
